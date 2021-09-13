40f3370254a7e465e238be18478f77d3263aaf69
Konstantin_999 <sh-tov@yandex.ru>
2021-09-13 23:52:16 +0700

    feat: добавить решение задач 11 и 12

--- 11-intersection/script.js
+++ 11-intersection/script.js
@@ -4,14 +4,21 @@
  * Напишите функцию intersection(arr1, arr2). Она должна принимать
  * на вход два массива целых чисел. Функция должна вернуть новый
  * массив чисел, содержащихся в обоих исходных массивах.
- * 
-*/
+ *
+ */
 
 function intersection(arr1, arr2) {
-    // Напишите код здесь
+  const setOfArr2 = new Set([...arr2]);
+  const intersectionArray = Array.from(new Set([...arr1.filter((item) => {
+    return setOfArr2.has(item);
+})]));
+    return intersectionArray;
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
 console.log(intersection([1, 5, 4, 2], [8, 91, 4, 1, 3])); // [4, 1]
 console.log(intersection([1, 5, 4, 2], [7, 12])); // []
+console.log(
+  intersection([1, 1, 0, 0, 0, 1, 5, 4, 2], [7, 12, 1, 1, 1, 1, 1, 0, 0, 0, 0])
+); // []
--- 12-count-zeros/script.js
+++ 12-count-zeros/script.js
@@ -7,10 +7,16 @@
 */
 
 function countZeros(n) {
+        let sumNull = 0;
+    for (i = 1; i <= n; i++) {
+        let string = String(i);
+        let  arrayOfNumber = Array.from(string);
+        let arrayNull = arrayOfNumber.filter(number => number === "0" );
+        sumNull = sumNull + arrayNull.length;
+    }
+    return sumNull;
     // Напишите код здесь
 }
-
 // Протестируйте решение, вызывая функцию с разными аргументами:
-
 console.log(countZeros(20)); // 2 – два нуля, по одному в числах 10 и 20
 console.log(countZeros(100)); // 11 – 11 нулей в числах: 10, 20, 30, 40, 50, 60, 70, 80, 90, 100
