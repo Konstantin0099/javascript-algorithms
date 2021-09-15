0402abdacbb80f3a604b5c37898770ad91292c82
Konstantin_999 <sh-tov@yandex.ru>
2021-09-15 10:19:10 +0700

    feat: добавить решение задачи №14

--- 14-primes/script.js
+++ 14-primes/script.js
@@ -5,10 +5,29 @@
  * Функция должна возвращать массив простых чисел от 2 до n.
  * 
 */
-
+function isPrime(n) {
+    if (n <= 1) {
+      return false;
+    } else {
+      for (let i = 2; i < n; i++) {
+        if (n % i === 0) {
+          return false;
+        }
+         } 
+         return true; 
+      }
+    }
 function primes(num) {
+    const array = [];
+    if (num < 2) { return array}
+    for (let number = 2; number <= num; number++) {
+       if (isPrime(number)) { 
+        array.push(number);
+    }
+       }
+       return array;
+    }
     // Напишите код здесь
-}
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
