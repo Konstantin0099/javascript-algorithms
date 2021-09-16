17e4934d245e12def0a975227657525a28ae05e8
Konstantin_999 <sh-tov@yandex.ru>
2021-09-16 22:18:57 +0700

    fix: исправить ошибку в алгоритме  задача 13

--- 13-sum-of-two/script.js
+++ 13-sum-of-two/script.js
@@ -9,45 +9,93 @@
  */
 
 function sumOfTwo(arr, sum) {
-    if (arr.length < 2) { return false; }
-    arr.sort((a, b) => { return a - b});
-  console.log(" стр 13 arr, sum", arr, sum);
+  console.log("входящий массив и сумма", arr, sum);
+  if (arr.length < 2) {
+    return false;
+  }
+  arr.sort((a, b) => {
+    return a - b;
+  });
   let j = 1;
   for (let i = 0; i < arr.length; i++) {
+    //первый цикл
+    console.log(
+      "первый цикл",
+      "arr[",
+      i,
+      "]=",
+      arr[i],
+      "+ arr[",
+      j,
+      "]=",
+      arr[j],
+      " sum = ",
+      arr[i] + arr[j]
+    );
     if (arr[i] + arr[j] >= sum) {
-      console.log(arr[i], arr[j], sum, "arr[", i, "]", "+ arr[", j, "]", "< sum");
-break;
+      break;
     }
     j = j + 1;
-  };
-  console.log("стр 22 arr[", j - 1, "]", "+ arr[", j, "] === sum", arr[j - 1], arr[j], sum);
+  }
   if (arr[j - 1] + arr[j] === sum) {
     return true;
-  };
+  }
   let i = j - 2;
+  console.log(
+    "второй цикл",
+    "arr[",
+    i,
+    "]=",
+    arr[i],
+    "+ arr[",
+    j,
+    "]=",
+    arr[j],
+    " sum = ",
+    arr[i] + arr[j]
+  );
   do {
-    console.log("стр 28 arr(", i, ") arr(", j, ") sum DO", arr[i], arr[j], sum);
+    // второй цикл
     if (i < 0 || j > arr.length) {
-        console.log("стр 30 return false"); return false;
-    };
+      return false;
+    }
     if (arr[i] + arr[j] === sum) {
-        return true;
-      };
+      return true;
+    }
     if (arr[i] + arr[j] >= sum) {
       i--;
     } else {
-      j--;
-    };
-    console.log("arr(i) + arr(j) проверка While", arr[i], arr[j], sum);
-  } while ((arr[i] + arr[j]) !== sum);
-  console.log("стр 39 arr(i) + arr(j) = sum ЗАВЕРШЕНИЕ WHILE", arr[i], arr[j], sum);
+      j++;
+    }
+    console.log(
+      "второй цикл",
+      "arr[",
+      i,
+      "]=",
+      arr[i],
+      "+ arr[",
+      j,
+      "]=",
+      arr[j],
+      " sum = ",
+      arr[i] + arr[j]
+    );
+  } while (arr[i] + arr[j] !== sum);
   return true;
-
-  // Напишите код здесь
 }
+
+
+console.log(sumOfTwo([1, 2, 4, 6, 8, 10], 11));
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 console.log(sumOfTwo([1, 2, 3, 4, 5], 4)); // true (так как 1 + 3 === 4)
 console.log(sumOfTwo([1, 2, 3, 4, 5], 100)); // false
-console.log(sumOfTwo([999, 999, 999, 1, 2, 3, 4, 5, 55, 55, 55, 55, 55, 55, 999, 999, 999, 1000, 1001, 1002], 1001)); // false
-
+console.log(
+  sumOfTwo(
+    [
+      999, 999, 999, 1, 2, 3, 4, 5, 55, 55, 55, 55, 55, 55, 999, 999, 999, 1000,
+      1001, 1002,
+    ],
+    1001
+  )
+); // false
